9aa01aab81ff1dffafa4dbd461cf5f35e8295998
Jarosław Piesak <jaroslawpiesak@gmail.com>
2024-03-17 20:03:35 +0100

    Zmiana klasy1

---
+++ Lab022/Lab022/Konflikt.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.AccessControl;
+
+namespace Lab022;
+
+public class Konflikt
+{
+    public static void Main1(string[] args)
+    {
+        int[] tab = {5,4,3,2,1};
+        int x = 0;
+        for (int i = 0; i < tab.Length; i++)
+        {
+            Console.WriteLine("Ile :" + tab[i] + " " + x);
+        }
+        
+    }
+}
--- Lab022/Lab022/obj/Debug/net8.0/Lab022.AssemblyInfo.cs
+++ Lab022/Lab022/obj/Debug/net8.0/Lab022.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab022")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8e53121e467e45cda612adc0f72571de2002616")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab022")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab022")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
